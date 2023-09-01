a4def313ea4e1c754b55dc7d36f7c299c4c82f75
Thiyaneshwaran04 <thiyaneshwaran123@gmail.com>
2023-08-31 20:03:54 -0500

    currencyconverter

---
+++ currencyconverter/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ConverterComponent } from './converter/converter.component';
+// import { CurrencyService } from './currency.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConverterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ currencyconverter/src/app/converter/converter.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+// import { CurrencyService } from '../currency.service';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.css']
+})
+export class ConverterComponent implements OnInit {
+currency=['USD','INR','EURO','AED','GBP']
+show=false
+option:any
+op1:any
+amount:any
+conveted:any
+a:any
+show1=true
+private exchangeRate:Record <string,number>={
+  USD:1,
+  INR:82.65,
+  EURO: 0.910651,
+  AED:3.67250,
+  GBP:0.785086
+};
+
+fromRate:any
+toRate:any
+fromCurrency:any
+toCurrency:any
+
+  constructor() {
+
+    
+  }
+  ngOnInit() { 
+  }
+  
+
+  convert(): void {
+    const fromRate = this.exchangeRate[this.option];
+    const toRate = this.exchangeRate[this.op1];
+    this. a=this.amount/fromRate*toRate
+    console.log(this.a);
+    this.show=true
+    this.show1=false
+    
+  }}
